1689efdaa60c314a08e090a010a0df33a8083165
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-12-30 10:41:10 +0600

    Implement logic for login

--- OrangeHRM/Login.py
+++ OrangeHRM/Login.py
@@ -14,14 +14,38 @@ def login_testCase1_valid():
 
     # Step 3: Enter Username
     username_field = driver.find_element(By.NAME, "username")
-    username_field.send_keys("Admin")
+    username_field_enable_state = username_field.is_enabled()
+
+    if username_field_enable_state:
+        username_field.clear()
+        username_field.send_keys("Admin")
+    else:
+        print("Username field is not enabled.Test Failed.")
+
     # Step 4: Enter Password
     password_field = driver.find_element(By.NAME, "password")
-    password_field.send_keys("admin123")
+    password_field_enable_state = password_field.is_enabled()
+
+    if password_field_enable_state:
+        password_field.clear()
+        password_field.send_keys("admin123")
+    else:
+        print("Password field is not enabled.Test Failed")
+
     # Step 5: Click Login button
     login_button = driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button")
     login_button.click()
     time.sleep(5)
+
+    # verify login or not
+    expected_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    actual_url = driver.current_url
+
+    if expected_url == actual_url:
+        print("Test Passed. Login successful.")
+    else:
+        print("Test Failed. Login failed.")
+
     driver.close()
 
 
@@ -36,14 +60,40 @@ def login_testCase2_invalid():
 
     # Step 3: Enter Username
     username_field = driver.find_element(By.NAME, "username")
-    username_field.send_keys("Admin123")
+    username_field_enable_state = username_field.is_enabled()
+
+    if username_field_enable_state:
+        username_field.clear()
+        username_field.send_keys("Admin123")
+    else:
+        print("Username field is not enabled.Test Failed.")
+
     # Step 4: Enter Password
     password_field = driver.find_element(By.NAME, "password")
-    password_field.send_keys("admin122333")
+    password_field_enable_state = password_field.is_enabled()
+
+    if password_field_enable_state:
+        password_field.clear()
+        password_field.send_keys("admin123")
+    else:
+        print("Password field is not enabled.Test Failed")
+
     # Step 5: Click Login button
     login_button = driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button")
     login_button.click()
     time.sleep(5)
+
+    # verify login or not by check error message
+    error_message = driver.find_element(By.CSS_SELECTOR, ".oxd-alert-content-text")
+    actual_error_message_text = error_message.text
+
+    expected_error_message = "Invalid credentials"
+
+    if expected_error_message == actual_error_message_text:
+        print("Test Passed. Login failed.Error message: " + expected_error_message)
+    else:
+        print("Test Failed. Did not get expected error message: " + expected_error_message)
+
     driver.close()
 
 
