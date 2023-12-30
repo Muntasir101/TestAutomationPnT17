9067209b5e9f463aa4c830fd56f6b4c2b1dfc20e
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-12-30 11:19:24 +0600

    Implement Logging

--- OrangeHRM/Login.py
+++ OrangeHRM/Login.py
@@ -1,57 +1,72 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import logging
+
+# configure the logging settings
+logging.basicConfig(filename="test_log.log", level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
 
 def login_testCase1_valid():
+    logging.info('login_testCase1_valid Execution Start....')
     # Step 1: Launch Browser
     driver = webdriver.Firefox()
+    logging.info('Firefox Launch Successful.')
     driver.maximize_window()
 
     # Step 2: Open URL
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     time.sleep(3)
+    logging.info('Login page open Successful.')
 
     # Step 3: Enter Username
     username_field = driver.find_element(By.NAME, "username")
     username_field_enable_state = username_field.is_enabled()
 
+    # verify username field is enabled or nor
     if username_field_enable_state:
         username_field.clear()
         username_field.send_keys("Admin")
+        logging.info('Type Username Successful.')
     else:
-        print("Username field is not enabled.Test Failed.")
+        logging.error("Username field is not enabled.Test Failed.")
 
     # Step 4: Enter Password
     password_field = driver.find_element(By.NAME, "password")
     password_field_enable_state = password_field.is_enabled()
 
+    # verify password field is enabled or not
     if password_field_enable_state:
         password_field.clear()
         password_field.send_keys("admin123")
+        logging.info('Type Password Successful.')
     else:
-        print("Password field is not enabled.Test Failed")
+        logging.error("Password field is not enabled.Test Failed")
 
     # Step 5: Click Login button
     login_button = driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button")
     login_button.click()
+    logging.info('Login Button clicked Successful.')
     time.sleep(5)
 
-    # verify login or not
+    # verify login or not by check URL
     expected_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
     actual_url = driver.current_url
 
     if expected_url == actual_url:
-        print("Test Passed. Login successful.")
+        logging.info('Test Passed. Login successful.')
     else:
-        print("Test Failed. Login failed.")
+        logging.error("Test Failed. Login failed.")
 
     driver.close()
+    logging.info('login_testCase1_valid execution completed..')
 
 
 def login_testCase2_invalid():
+    logging.info('login_testCase2_invalid Execution Start....')
     # Step 1: Launch Browser
     driver = webdriver.Firefox()
+    logging.info('Firefox Launch Successful.')
     driver.maximize_window()
 
     # Step 2: Open URL
@@ -62,25 +77,30 @@ def login_testCase2_invalid():
     username_field = driver.find_element(By.NAME, "username")
     username_field_enable_state = username_field.is_enabled()
 
+    # verify that username field is enabled or not
     if username_field_enable_state:
         username_field.clear()
         username_field.send_keys("Admin123")
+        logging.info('Type Username Successful.')
     else:
-        print("Username field is not enabled.Test Failed.")
+        logging.error("Username field is not enabled.Test Failed.")
 
     # Step 4: Enter Password
     password_field = driver.find_element(By.NAME, "password")
     password_field_enable_state = password_field.is_enabled()
 
+    # verify that password field is enabled or not
     if password_field_enable_state:
         password_field.clear()
         password_field.send_keys("admin123")
+        logging.info('Type Password Successful.')
     else:
-        print("Password field is not enabled.Test Failed")
+        logging.error("Password field is not enabled.Test Failed")
 
     # Step 5: Click Login button
     login_button = driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button")
     login_button.click()
+    logging.info('Login Button clicked Successful.')
     time.sleep(5)
 
     # verify login or not by check error message
@@ -90,12 +110,13 @@ def login_testCase2_invalid():
     expected_error_message = "Invalid credentials"
 
     if expected_error_message == actual_error_message_text:
-        print("Test Passed. Login failed.Error message: " + expected_error_message)
+        logging.info("Test Passed. Login failed.Error message: " + expected_error_message)
     else:
-        print("Test Failed. Did not get expected error message: " + expected_error_message)
+        logging.error("Test Failed. Did not get expected error message: " + expected_error_message)
 
     driver.close()
+    logging.info('login_testCase2_invalid execution completed..')
 
 
 login_testCase1_valid()
-login_testCase2_invalid()
+#login_testCase2_invalid()
